9b5dcf263fe902a56c2f507a4e0170f45af3ab4d
tim hau <tim.hau@hotmail.de>
2021-03-03 18:06:30 +0100

    add dataset struct

--- src/main.cpp
+++ src/main.cpp
@@ -11,6 +11,12 @@
 
 namespace fs = std::filesystem;
 
+struct Dataset {
+	const fs::path xml_path;
+	const fs::path config_path;
+	std::vector<Object> objects;
+};
+
 std::string ask()
 {
 	std::cout << "................................." << std::endl;
@@ -30,6 +36,7 @@ int main()
 {
 	std::cout << ".............Welcome............." << std::endl;
 
+	std::vector<Dataset> datasets;
 	for (auto& dir : fs::directory_iterator("../data/datasets")) {
 		if (!fs::is_directory(dir))
 			continue;
@@ -48,8 +55,27 @@ int main()
 				Object object{ obj };
 				objects.emplace_back(object);
 			}
+
+			std::string xml_filename = config_file.path().stem().string() + ".xml";
+			const fs::path& xml_path = dataset_path / "xml" / xml_filename;
+			Dataset d{xml_path, config_path, objects};
+			datasets.push_back(d);
 		}
 	}
+
+	// for debugging only
+	// ---------------------------------------------------------------------
+	auto obj_a = datasets[1].objects[44];
+	auto obj_b = datasets[1].objects[3];
+	auto rel_ab = obj_a.intrinsic_orientation_to(obj_b);
+	if (rel_ab)
+	{
+		std::cout << obj_a.get_id()->c_str() << " " << rel_ab.value() << " " << obj_b.get_id()->c_str()
+				  << std::endl;
+		std::cout << "-------" << std::endl;
+	}
+	std::cout << "obj a:" << *(obj_a.get_id()) << "_" << *(obj_a.get_label()) << " obj_b " << *(obj_b.get_id()) << "_" << *(obj_b.get_label()) << std::endl;
+	// ---------------------------------------------------------------------
 
 	bool should_stop = false;
 	while (!should_stop)
@@ -64,44 +90,23 @@ int main()
 
 		if (input == "x")
 		{
-			/*
-			fs::path xml_output_path_matterport("../data/datasets/matterport3d/xml/matterport3d.xml");
-			XmlExporter::to_xml(xml_output_path_matterport, objects_matterport);
-			 */
+			for (auto& dataset : datasets)
+			{
+				XmlExporter::to_xml(dataset.xml_path, dataset.objects);
+			}
 		}
 
 		if (input == "d")
 		{
-			DebugExporter::to_ply("../data/datasets/sunc/config/");
-			DebugExporter::to_ply("../data/datasets/matterport3d/config/");
+			for (auto& dataset : datasets)
+			{
+				DebugExporter::to_ply(dataset.config_path);
+			}
 		}
 
 		if (input == "q")
-		{
 			should_stop = true;
-		}
 	}
-
-
-	auto config = cpptoml::parse_file("../data/datasets/sunc/config/00a2a04afad84b16ff330f9038a3d126.toml");
-	auto val = config->get_qualified_as<std::string>("dataset.name");
-	std::vector<Object> objects_matterport;
-	for (const auto& obj : *config->get_table_array("object"))
-	{
-		Object object{ obj };
-		objects_matterport.emplace_back(object);
-	}
-
-	auto obj_a = objects_matterport[3];
-	auto obj_b = objects_matterport[50];
-	auto rel_ab = obj_a.intrinsic_orientation_to(obj_b);
-	if (rel_ab)
-	{
-		std::cout << obj_a.get_id()->c_str() << " " << rel_ab.value() << " " << obj_b.get_id()->c_str()
-				  << std::endl;
-		std::cout << "-------" << std::endl;
-	}
-	std::cout << "obj a:" << *(obj_a.get_id()) << "_" << *(obj_a.get_label()) << " obj_b " << *(obj_b.get_id()) << "_" << *(obj_b.get_label()) << std::endl;
 
 	return 0;
 }
